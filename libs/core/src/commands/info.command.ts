8ae56d09c81e1d1b0b4ee7fdf36039e9450cc4b7
Ilya Mixaltik <mixaltik16@gmail.com>
2022-06-14 15:44:08 +0300

    update info command

--- libs/core/src/commands/info.command.ts
+++ libs/core/src/commands/info.command.ts
@@ -1,10 +1,8 @@
-export class InfoCommand {
-  constructor() {
-    let info: string = `Инфо о проекте:\n`;
-    info += `Version: 1.0.0\n`
-    info += `NodeJS: v18.3.0\n`
-    info += `TypeScript: v4.7.3\n`
+export const InfoCommand = () => {
+  let info: string = `Инфо о проекте:\n`;
+  info += `Version: 1.0.0\n`
+  info += `NodeJS: v18.3.0\n`
+  info += `TypeScript: v4.7.3\n`
 
-    return info;
-  }
+  return info;
 }

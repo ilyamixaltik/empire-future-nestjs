b00e8e351104a3b86dca5a2e820ea6e5e0c0daf6
Ilya Mixaltik <mixaltik16@gmail.com>
2022-06-14 16:22:46 +0300

    update service vk module

--- apps/vk-svc/src/vk-svc.module.ts
+++ apps/vk-svc/src/vk-svc.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CoreService } from '@app/core';
+import { VkModule } from 'nestjs-vk';
 import { VkSvcController } from './vk-svc.controller';
 import { VkSvcService } from './vk-svc.service';
 
@@ -8,9 +9,16 @@ import { VkSvcService } from './vk-svc.service';
   imports: [
     ConfigModule.forRoot({
       envFilePath: 'process.env',
+    }),
+    VkModule.forRoot({
+      token: process.env.VK_BOT_TOKEN,
+      options: {
+        pollingGroupId: +process.env.VK_BOT_GROUP_ID,
+        apiMode: 'sequential',
+      },
     })
   ],
-  controllers: [VkSvcController],
-  providers: [VkSvcService, CoreService],
+  controllers: [],
+  providers: [VkSvcService, VkSvcController, CoreService],
 })
 export class VkSvcModule {}

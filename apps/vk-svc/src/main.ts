698cec0f21ea1c46820deade53d482d5218bdf1f
Ilya Mixaltik <mixaltik16@gmail.com>
2022-06-14 16:47:59 +0300

    fix process env vk

--- apps/vk-svc/src/main.ts
+++ apps/vk-svc/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { VkSvcModule } from './vk-svc.module';
 
 async function bootstrap() {
-  const port = Number(process.env.VK_SERVICE_PORT_PORT);
+  const port = Number(process.env.TELEGRAM_SERVICE_PORT);
   const app = await NestFactory.create(VkSvcModule);
 
   await app.listen(port);

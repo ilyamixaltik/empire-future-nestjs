5754f2fa656b46f9a0414b40283c5eb3720dab78
Ilya Mixaltik <mixaltik16@gmail.com>
2022-06-14 16:05:12 +0300

    create connect config module for vk service module

--- apps/vk-svc/src/vk-svc.module.ts
+++ apps/vk-svc/src/vk-svc.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CoreService } from '@app/core';
 import { VkSvcController } from './vk-svc.controller';
 import { VkSvcService } from './vk-svc.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'process.env',
+    })
+  ],
   controllers: [VkSvcController],
   providers: [VkSvcService, CoreService],
 })

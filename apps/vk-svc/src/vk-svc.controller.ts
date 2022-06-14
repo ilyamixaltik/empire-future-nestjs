b574434fa5925207b6d5179e97c718d4868540bb
Ilya Mixaltik <mixaltik16@gmail.com>
2022-06-14 15:51:25 +0300

    create info command

--- apps/vk-svc/src/vk-svc.controller.ts
+++ apps/vk-svc/src/vk-svc.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Ctx, Hears } from 'nestjs-vk';
+import { MessageContext } from 'vk-io';
+import { Controller } from '@nestjs/common';
 import { VkSvcService } from './vk-svc.service';
 
 @Controller()
 export class VkSvcController {
   constructor(private readonly vkSvcService: VkSvcService) {}
 
-  @Get()
-  getHello(): string {
-    return this.vkSvcService.getHello();
+  @Hears(/^\/?(info|инфо)$/i)
+  async onInfoCommand(@Ctx() ctx: MessageContext) {
+    const data = this.vkSvcService.onInfoCommand(ctx);
+
+    return ctx.reply(data);
   }
 }
--- apps/vk-svc/src/vk-svc.module.ts
+++ apps/vk-svc/src/vk-svc.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CoreService } from '@app/core';
 import { VkSvcController } from './vk-svc.controller';
 import { VkSvcService } from './vk-svc.service';
 
 @Module({
   imports: [],
   controllers: [VkSvcController],
-  providers: [VkSvcService],
+  providers: [VkSvcService, CoreService],
 })
 export class VkSvcModule {}
